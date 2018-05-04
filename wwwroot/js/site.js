3ed99bd37f93c755ca6324b071c73883046b0269
hartiparti <34035433+hartiparti@users.noreply.github.com>
2018-05-04 01:53:47 +0000

    Harti

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,14 +1,1 @@
 ﻿// Write your JavaScript code.
-
-function truncateText(selector, maxLength) {
-    var element = document.querySelector(selector),
-        truncated = element.innerText;
-
-    if (truncated.length > maxLength) {
-        truncated = truncated.substr(0,maxLength) + '...';
-    }
-    return truncated;
-}
-
-
-document.querySelector('p').innerText = truncateText('p', 107);
